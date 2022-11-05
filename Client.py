c3427a6b3d3cac23be53296ffc355adb8d811e43
johnglass2003 <50562358+johnglass2003@users.noreply.github.com>
2022-11-05 14:39:54 -0400

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,5 +1,3 @@
-# echo-client.py
-
 import socket
 import sys
 import PySimpleGUI as sg
@@ -8,32 +6,9 @@ import PySimpleGUI as sg
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-#set layout
-layout = [
-    [sg.Graph(canvas_size=(400, 400), graph_bottom_left=(0, 0), graph_top_right=(400, 400), background_color='red', enable_events=True, key='graph')],
-    [sg.Text("Hello from PySimpleGUI")], [sg.Button('1', key=lambda: func('Button 1 pressed'))]
-]
-
-#create PySimpleGUI window
-window = sg.Window("Demo", layout,size=(1250, 750), resizable=True, finalize=True)
-
-#create graph
-graph = window['graph']         # type: sg.Graph
-#create point in graph
-point = graph.draw_point((25, 25), 10, color='green')
-
-#for creating a list of people in museum
-#people = 5
-#peopleList = []
-#for i in range(people):
-#    point = graph.draw_point((25, 25), 10, color='green')
-#    peopleList.append(point)
-# can also use keys and element = window[key] <-- possibly better approach
-
 #test button for pressing
 def func(message='Default message'):
     print(message)
-
 
 #creating socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -41,7 +16,50 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.listen()
     conn, addr = s.accept()
     with conn:
+        #audio files list
+        audioFiles = []
+        exhibits = []
+
         print(f"Connected by {addr}")
+
+        #recieve, decode and add audio files and exhibits from server into list
+        data = conn.recv(1024)
+        data_decoded = data.decode('utf-8')
+        data_decoded = data_decoded.replace('[', '')
+        data_decoded = data_decoded.replace(']', '')
+        data_decoded = data_decoded.replace(',', '')
+        data_decoded = data_decoded.replace('\'', '')
+        lists = data_decoded.split("^")
+        pos1 = lists[0].split()
+        pos2 = lists[1].split()
+        for p1 in pos1:
+            audioFiles.append(p1)
+        for p2 in pos2:
+            exhibits.append(p2)
+
+        #set layout
+        layout = [
+            [sg.Graph(canvas_size=(400, 400), graph_bottom_left=(0, 0), graph_top_right=(400, 400), background_color='red', enable_events=True, key='graph')],
+            [sg.Text("Hello from PySimpleGUI")], [sg.Button('1', key=lambda: func('Button 1 pressed'))],
+            [sg.Combo(audioFiles, size = (10,1), key='Audio')],
+            [sg.Combo(exhibits, size = (10,1), key='Exhibits')]
+        ]
+
+        #create PySimpleGUI window
+        window = sg.Window("Demo", layout,size=(1250, 750), resizable=True, finalize=True)
+
+
+        #create graph
+        graph = window['graph']         # type: sg.Graph
+        #create point in graph
+        point = graph.draw_point((25, 25), 10, color='green')
+        #for creating a list of people in museum
+        #people = 5
+        #peopleList = []
+        #for i in range(people):
+        #    point = graph.draw_point((25, 25), 10, color='green')
+        #    peopleList.append(point)
+        # can also use keys and element = window[key] <-- possibly better approach
         while True:
             event, values = window.read(timeout=10)
             if callable(event):
@@ -63,7 +81,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     x = int(pos[0].split('.')[0])
                     y = int(pos[1].split('.')[0])
                     #move point
-                    graph.MoveFigure(point, x, y)
+                    graph.relocate_figure(point, x, y)
                     
             except OSError:
                 s.close()
