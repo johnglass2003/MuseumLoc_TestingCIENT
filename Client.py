efda1611720926a95f5abab8ffeef2e9cdd234ff
John Glass <50562358+johnglass2003@users.noreply.github.com>
2024-03-28 16:34:27 -0400

    Update Client.py

--- Client.py
+++ Client.py
@@ -5,7 +5,7 @@ import PySimpleGUI as sg
 
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+PORT = 65431  # Port to listen on (non-privileged ports are > 1023)
 
 #SSH sending information
 host = "127.0.0.1"   #server host
